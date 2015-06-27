d5b5c05395e9a354345f05ecd60c3ef9dd78e96f
Angus Thomsen <aksthomsen@gmail.com>
2015-06-27 18:56:41 +1000

    config variables

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
+Meteor.startup(function () {
+  Twilio = Twilio(Config.ACCOUNT_SID, Config.AUTH_TOKEN);
+})
+
 Meteor.methods({
   sendSpam: function (payload) {
+    if (payload.mobile) {
+      console.log('sending message to', payload.mobile);
+    }
     console.log(payload.twitter, payload.mobile, payload.email);
   }
 });
